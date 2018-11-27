76cce14f4e5bd3ae38d692b892efa99788470816
g0josh <jobin.jj@gmail.com>
2018-11-27 16:45:15 +1100

    video feed working, but slow

--- src/detector.py
+++ src/detector.py
@@ -93,7 +93,7 @@ def run_first_stage(image, net, scale, threshold):
     """
     width, height = image.size
     sw, sh = math.ceil(width*scale), math.ceil(height*scale)
-    img = image.resize((sw, sh), Image.BILINEAR)
+    img = image.resize((int(sw), int(sh)), Image.BILINEAR)
     img = np.asarray(img, 'float32')
     img = torch.FloatTensor(_preprocess(img))
 
--- test.py
+++ test.py
@@ -1,12 +1,26 @@
+from __future__ import print_function
 from src.detector import detect_faces
 from utils.visualization_utils import show_bboxes
 from PIL import Image
+import cv2
+import numpy
 
 def main():
-    image = Image.open('images/test3.jpg')
-    bounding_boxes, landmarks = detect_faces(image)
-    image = show_bboxes(image, bounding_boxes, landmarks)
-    image.show()
+    #image = Image.open('images/test2.jpg')
+    cap = cv2.VideoCapture(0)
+    cap.set(3,1280)
+    cap.set(4,720)
+    while cap.isOpened():
+        isSuccess, frame = cap.read()
+        if isSuccess:
+            image = Image.fromarray(frame)
+            bounding_boxes, landmarks = detect_faces(image)
+            #print("bbox = {}\n{}\nland={}\n{}".format(type(bounding_boxes), bounding_boxes, type(landmarks), landmarks))
+            image = show_bboxes(image, bounding_boxes, landmarks)
+            #image.show()
+            cv2.imshow('face cap', numpy.array(image))
+            if cv2.waitKey(1)&0xFF == ord('q'):
+                break
 
 if __name__ == "__main__":
     main()
